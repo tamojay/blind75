a16268ddc85f4a86262ad9a8e03dd9f29f4acd6b
Tamojay Dey <tamojay@interpl.ai>
2022-10-22 13:11:34 +0530

    initial commit

---
+++ slidingWindow/minimumWindowSubstring.js
@@ -0,0 +1,10 @@
+/* Leetcode 76 */
+
+function minimumWindowSubstring(s, t) {
+
+}
+
+const big = 'ADOBECODEBANC'
+const small = 'ABC'
+
+console.log(minimumWindowSubstring(big, small))  // Output - 'BANC'
